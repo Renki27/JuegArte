87e673777207ea815876c333c78029246fe2377e
Renki27 <emilio.rojas@ucrso.info>
2020-10-10 14:48:07 -0600

    Question Controller

--- app/src/main/java/com/dam/juegarte/ScratchGame.java
+++ app/src/main/java/com/dam/juegarte/ScratchGame.java
@@ -6,15 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.dam.juegarte.controller.QuestionController;
+
 public class ScratchGame extends AppCompatActivity{
 
     FragmentTransaction transaction;
     Fragment fragmentScratch;
+    QuestionController questionController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scratch_game);
+
+        questionController = new QuestionController(this);
+
+        questionController.loadScratchQuestions();
 
      //   fragmentScratch = new ScratchFragment();
      //   getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, fragmentScratch).commit();
--- app/src/main/java/com/dam/juegarte/controller/QuestionController.java
+++ app/src/main/java/com/dam/juegarte/controller/QuestionController.java
@@ -13,31 +13,33 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.dam.juegarte.GameMode;
-import com.dam.juegarte.stores.GameModesStore;
+import com.dam.juegarte.Question;
 import com.dam.juegarte.R;
+import com.dam.juegarte.ScratchQuestion;
+import com.dam.juegarte.stores.GameModesStore;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class GameController {
+public class QuestionController {
 
     //	10.0.2.2 = Special alias to your host loopback interface (i.e., 127.0.0.1 on your development machine)
     private static final String BASE_URL = "http://10.0.2.2/juegarte-API";
     private Context context;
 
-    public GameController(Context context) {
+    public QuestionController(Context context) {
         this.context = context;
     }
 
 
-    public void loadGameModes() {
+    public void loadScratchQuestions() {
 
         RequestQueue requestQueue = Volley.newRequestQueue(context);
 
 
-        StringRequest request = new StringRequest(Request.Method.GET, BASE_URL + "/loadGameModes.php?request=loadGameModes",
+        StringRequest request = new StringRequest(Request.Method.GET, BASE_URL + "/loadQuestions.php?request=loadScrQue",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -49,26 +51,35 @@ public class GameController {
 
 
                             if (status.equals("Success")) {
-                                JSONArray jsonArray = jsonObject.getJSONArray("gameModes");
-                                ArrayList<GameMode> gameModes = new ArrayList<>();
+                                JSONArray jsonArray = jsonObject.getJSONArray("scratchQuestions");
+                                ArrayList<Question> scratchQuestions = new ArrayList<>();
 
 
                                 for (int i = 0; i < jsonArray.length(); i++) {
-                                    JSONObject modes = jsonArray.getJSONObject(i);
+                                    JSONObject questions = jsonArray.getJSONObject(i);
 
 
-                                    int idGame = Integer.parseInt(modes.getString("id_game").trim());
-                                    String gameMode = modes.getString("game_mode").trim();
-                                    String instructions = modes.getString("instructions").trim();
-                                    int questionTimer = Integer.parseInt(modes.getString("question_timer_seconds").trim());
+
+                                    String questionText = questions.getString("question_text").trim();
+                                    String questionInformation = questions.getString("question_info").trim();
+                                    String questionImage = questions.getString("question_img").trim();
+                                    String option1 = questions.getString("option1").trim();
+                                    String option2 = questions.getString("option2").trim();
+                                    String option3 = questions.getString("option3").trim();
+                                    String option4 = questions.getString("option4").trim();
+                                    String answer = questions.getString("answer").trim();
 
 
-                                    GameMode mode = new GameMode(idGame, gameMode, instructions, questionTimer);
-                                    gameModes.add(mode);
+
+
+                                    Question question = new ScratchQuestion(questionText, questionInformation, questionImage, option1, option2, option3, option4, answer);
+                                    scratchQuestions.add(question);
 
                                 }
-                                GameModesStore gameModesStore = new GameModesStore(context);
-                                gameModesStore.storeGameModes(gameModes);
+
+                                Log.d("Scratch", scratchQuestions.toString());
+                               // GameModesStore gameModesStore = new GameModesStore(context);
+                               // gameModesStore.storeGameModes(gameModes);
 
 
 
