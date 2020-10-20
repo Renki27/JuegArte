99b62929952e183cb6cb48472fb657d3a19d7359
Brandon Cisneros <bjcr2726@gmail.com>
2020-10-20 10:46:20 -0600

    Account Controller initializaed test

--- app/src/test/java/com/dam/juegarte/controller/AccountControllerTest.java
+++ app/src/test/java/com/dam/juegarte/controller/AccountControllerTest.java
@@ -3,9 +3,11 @@ package com.dam.juegarte.controller;
 import junit.framework.TestCase;
 
 public class AccountControllerTest extends TestCase {
+    private AccountController accountController;
 
     public void setUp() throws Exception {
         super.setUp();
+
     }
 
     public void tearDown() throws Exception {
