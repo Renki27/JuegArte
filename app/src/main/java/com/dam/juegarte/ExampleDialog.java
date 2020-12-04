90c6c2784a381044b4adadba1cfa6818c8bb570d
LuisAlv3001 <63750558+LuisAlv3001@users.noreply.github.com>
2020-12-03 22:50:25 -0600

    Settings

---
+++ app/src/main/java/com/dam/juegarte/ExampleDialog.java
@@ -0,0 +1,28 @@
+package com.dam.juegarte;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.about_us)
+                .setMessage("aquí va una descripción miedo")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
